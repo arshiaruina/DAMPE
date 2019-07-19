b4a72f7319b3e7a0c283adcb37e92a789010067e
Arshia Ruina <arshia.ruina@unige.ch>
2019-07-19 20:55:00 +0200

    CalcEta seems to give expected results - feature to read input files from a list implemented - debugged main() and CalcEta() - caveat: eta for cluster size 1 set to 0 (biased?) - next: plot eta vs. energy

--- He_flux_analysis/src/eta.cc
+++ He_flux_analysis/src/eta.cc
@@ -40,6 +40,7 @@
 #include "TStopwatch.h"
 #include "DmpStkClusterCalibration.h"
 
+
 //#include "track_selection.hpp"
 //#include "etacorr.hpp"
 
@@ -50,36 +51,91 @@ using namespace std;
 
 int main() {
 
-	gSystem->Load("./libDmpEvent.so");
+        std::ifstream listOfFiles;
+	listOfFiles.open("../resources/20181019.txt"); // one day data
 
-	string inputfile = "/beegfs/dampe/prod/FM/FlightData/2A/20181019/DAMPE_2A_OBS_20181019_20181019T232028_20181019T233706_00000_cu6fCNZZzBuq1VAgiuIw/DAMPE_2A_OBS_20181019_20181019T232028_20181019T233706_00000_cu6fCNZZzBuq1VAgiuIw.root"; 
+        if(!listOfFiles.good()){ // Returns true if none of the stream's error state flags (eofbit, failbit and badbit) is set.
+                std::cout << "Problem with list of files!" << std::endl;
+                return 1;
+        }
 
-	TFile *f = new TFile(inputfile.c_str());
-	TTree *t = (TTree*)f->Get("CollectionTree");
+	int nFiles = 0;
 
-	//DmpStkSiCluster *fCluster = 0;
-	//t->SetBranchAddress("DmpStkSiCluster", &fCluster);
+        //while(!listOfFiles.eof() && nFiles<1) { // uncomment for analysis run
+        while(!listOfFiles.eof() && nFiles<1) { // uncomment for debug run
+        
+		std::string fileName;
+	        std::getline(listOfFiles, fileName);
 
-TClonesArray* stkclusters  = new TClonesArray("DmpStkSiCluster");
-t->SetBranchAddress("StkClusterCollection",stkclusters); // name o fthe branch
+		TFile *f = new TFile(fileName.c_str());
+		std::cout << "Reading file " << fileName.c_str() << std::endl; 
 
-TClonesArray* stktracks = new TClonesArray("DmpStkTrack"); //name of the class
-t->SetBranchAddress("StkKalmanTracks",stktracks); // name o fthe branch
-//DmpStkTrackHelper(TClonesArray* stktracks, bool usebgo = false, DmpEvtBgoRec* bgorec=0, DmpEvtBgoHits* bgohits=0):
+		TTree *t = (TTree*)f->Get("CollectionTree");
+
+		TClonesArray* stktracks = new TClonesArray("DmpStkTrack"); //name of the class
+		t->SetBranchAddress("StkKalmanTracks",&stktracks); // name of the branch
+		//DmpStkTrackHelper(TClonesArray* stktracks, bool usebgo = false, DmpEvtBgoRec* bgorec=0, DmpEvtBgoHits* bgohits=0):
+
+		TClonesArray* stkclusters  = new TClonesArray("DmpStkSiCluster"); // name of the class
+		t->SetBranchAddress("StkClusterCollection",&stkclusters); // name of the branch
+
+		std::cout << std::endl;
+		std::cout << "No. of events " << t->GetEntries() << std::endl;
+
+		//for (int i = 0; i < t->GetEntries(); i++){ // uncomment for analysis run
+		for (int i = 0; i < 1; i++){ // uncomment for debug run
+	
+			std::cout << std::endl;
+			std::cout << "Processing event " << i+1 << std::endl;
+			t->GetEntry(i);
+			
+			std::cout << std::endl;
+			std::cout << "No. of tracks in this event " << stktracks->GetLast()+1 << std::endl;
+			
+			for(int itrack = 0; itrack <= stktracks->GetLast(); itrack++){
+
+				std::cout << std::endl;
+				std::cout << "Processing track " << itrack+1 << " of event " << i+1 << std::endl;
+				DmpStkTrack* stktrack = (DmpStkTrack*) stktracks->ConstructedAt(itrack);
+				
+				std::cout << std::endl;
+				std::cout << "No. of clusters in this track " << stktrack->GetNPoints() << std::endl;
+				
+				std::cout << "stktrack->getNhitXY() " << stktrack->getNhitXY() << std::endl;
+				std::cout << "stktrack->getNhitX() " << stktrack->getNhitX() << std::endl;
+				std::cout << "stktrack->getNhitY() " << stktrack->getNhitY() << std::endl;
+				std::cout << "stktrack->getImpactPoint() " << "[" << stktrack->getImpactPoint().X() << "," << stktrack->getImpactPoint().Y() << "," << stktrack->getImpactPoint().Z() << "]" << std::endl;
+				std::cout << "stktrack->getImpactPointHasX() " << stktrack->getImpactPointHasX() << std::endl;
+				std::cout << "stktrack->getImpactPointHasY() " << stktrack->getImpactPointHasY() << std::endl;
+				std::cout << "stktrack->isImpactPointLayerX() " << stktrack->isImpactPointLayerX() << std::endl;
+				std::cout << "stktrack->isImpactPointLayerY() " << stktrack->isImpactPointLayerY() << std::endl;
+
+				double clusterEnergy = 0.;
+				double cosTheta = stktrack->getDirection().CosTheta();
+				double clusterEta = 0.;
+				
+				for (int icluster = 0; icluster < stktrack->GetNPoints(); icluster++) {
+				
+					std::cout << std::endl;
+					std::cout << "Processing cluster " << icluster+1 << " of track " << itrack+1 << " of event " << i+1 << std::endl;
+					
+					DmpStkSiCluster* stkcluster = (DmpStkSiCluster*) stkclusters->ConstructedAt(icluster);
+					std::cout << "debug 1" << std::endl; 
+					clusterEnergy = stkcluster->getEnergy()*cosTheta;
+					std::cout << "debug 2" << std::endl; 
+					if(stkcluster->getNstrip()==0) continue;
+					clusterEta = CalcEta(stkcluster);
+					std::cout << "debug 3" << std::endl; 
+
+				} // end of loop over clusters
+			} // end of loop over tracks
+		} // end of loop over events
+		nFiles++;
+	} // end of loop over files	
+} // end of main
 
 
-
-	int nEvents = t->GetEntries();
-	for (int i = 0; i < nEvents; i++) {
-
-		t->GetEntry(i);
-		for(int itrack=0; itrack<=stktracks->GetLast();++itrack){
-			DmpStkTrack* stktrack = (DmpStkTrack*) stktracks->ConstructedAt(itrack);
-			for (int ipoint=0; ipoint<stktrack->GetNPoints(); ipoint++) {
-			}
-		}
-
-
+/*
             // Loop over clusters
             for (int ipoint=0; ipoint<stktrack->GetNPoints(); ipoint++) {
                 for (int ixy=0; ixy<2; ixy<++) {
@@ -102,24 +158,27 @@ t->SetBranchAddress("StkKalmanTracks",stktracks); // name o fthe branch
 	return 0;
 }
 
+*/
 
 double CalcEta(DmpStkSiCluster* cluster){
   
 	// Eta has to be calculated for each cluster associated with the selected track
 
+	//std::cout << "CalcEta::debug 1" << std::endl; 
 	// initialization
 	double sig = 0.;
 	double sigMax1 = 0.;
 	double sigMax2 = 0.;
-	int nstrips = cluster->getNstrip(); // number of strips
+	int nStrips = cluster->getNstrip(); // number of strips
 	int stripMax1 = 0;
 	int stripMax2 = 0;
 	int ch1 = 0;
 	int ch2 = 0;
 	double eta = 0.;
 	
+	//std::cout << "CalcEta::debug 2" << std::endl; 
 	// finding highest signal
-	for(int istrip = 0; istrip < nstrips; istrip++){
+	for(int istrip = 0; istrip < nStrips; istrip++){
 		sig = cluster->GetSignal(istrip);
 		if(sig > sigMax1){
 			sigMax1 = sig;
@@ -128,22 +187,40 @@ double CalcEta(DmpStkSiCluster* cluster){
 		ch1++;
 	}
 	
+	//std::cout << "CalcEta::debug 3" << std::endl; 
+	std::cout << "Size of cluster " << nStrips << std::endl; 
+	std::cout << "Strip with highest signal " << stripMax1 << std::endl; 
+	//std::cout << cluster->GetSignal(stripMax1-1) << std::endl; 
+	std::cout << "Highest signal " << cluster->GetSignal(stripMax1) << std::endl; 
+	//std::cout << cluster->GetSignal(stripMax1+1) << std::endl; 
 	// finding adjacent strip with second-highest signal
-	if(cluster->GetSignal(stripMax1 - 1) > cluster->GetSignal(stripMax1 + 1)
-		stripMax2 = stripMax1 - 1;
-	else
-		stripMax2 = stripMax1 + 1;
-	
-	sigMax2 = cluster->GetSignal(stripMax2);
-	
+	//if(nStrips==1) --> then eta is set to 0. (biased)
+	if(nStrips==2) {
+		stripMax2 = !stripMax1;
+                sigMax2 = cluster->GetSignal(stripMax2);
+	}
+	else {
+		if(cluster->GetSignal(stripMax1 - 1) > cluster->GetSignal(stripMax1 + 1))
+			stripMax2 = stripMax1 - 1;
+		else
+			stripMax2 = stripMax1 + 1;
+		sigMax2 = cluster->GetSignal(stripMax2);
+	}
+	//std::cout << "CalcEta::debug 4" << std::endl; 
+	std::cout << "Strip with second highest signal " << stripMax2 << std::endl; 
+	std::cout << "Second highest signal " << cluster->GetSignal(stripMax2) << std::endl; 
 	// compute eta
-	ch1 = GetChannelID(stripMax1);
-	ch2 = GetChannelID(stripMax2);
-	if(ch1 < ch2)
+	ch1 = cluster->GetChannelID(stripMax1);
+	ch2 = cluster->GetChannelID(stripMax2);
+	std::cout << "Channel ID of strip with highest signal " << ch1 << std::endl; 
+	std::cout << "Channel ID of strip with second highest signal " << ch2 << std::endl; 
+	if(ch1 > ch2)
 		eta = sigMax1/(sigMax1 + sigMax2);
 	else
 		eta = sigMax2/(sigMax1 + sigMax2);
 	
+	std::cout << "Eta for this cluster " << eta << std::endl; 
+	//std::cout << "CalcEta::debug 5" << std::endl; 
 	return eta;
 }
 
