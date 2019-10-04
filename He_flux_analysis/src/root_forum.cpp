c80e7d8d9e1d78f662ab4a6d83039d08d755becb
Arshia Ruina <arshia.ruina@unige.ch>
2019-10-04 20:30:15 +0200

    Code to access histograms iteratively inside a root file - debugged with the help of ROOT forum usersÂ! - https://root-forum.cern.ch/t/accessing-all-histograms-inside-a-rootfile/36070

---
+++ He_flux_analysis/src/root_forum.cpp
@@ -0,0 +1,57 @@
+/* Code to compute eta values
+ * Author: Arshia Ruina
+ * To compile, g++ -Wall `root-config --cflags --libs` -I$DMPSWSYS/include -L$DMPSWSYS/lib -lDmpEvent -lDmpService eta.cc -o ../submit/eta.exe
+ * */
+
+// example file
+// /beegfs/users/ruina/out/20181019/merged/merged_160919_104734.root
+
+#include "../inc/va_equalisation.h"
+#include "mylangaus.C"
+#include "TKey.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    std::string inFileName = argv[1];
+    TFile *inFile = new TFile(inFileName.c_str());
+    TList *list = inFile->GetListOfKeys();
+    TFile *outFile = new TFile("../out/root_forum.root", "recreate");
+
+    //------------------------------------------------//
+
+    //string classname = "TH1D";
+    //TH1D *hist;
+    //TIter iter(list); //or TIter iter(list->MakeIterator());
+    //while(hist = (TH1D*)iter()){
+    //    if(hist->ClassName() == classname.c_str()){
+    //        outFile->WriteTObject(hist);
+    //    }
+    //}
+
+
+    //------------------------------------------------//
+    
+    TKey *key;
+    TIter iter(list); //or TIter iter(list->MakeIterator());
+    static TString classname("TH1D");
+    while((key = (TKey*)iter())) {
+        if (key->GetClassName() == classname) {
+            TH1D *hist = (TH1D*)key->ReadObj();
+            if (hist) {
+                std::cout << hist->GetEntries() << std::endl;
+                outFile->WriteTObject(hist);
+                delete hist;
+            }
+        }
+    }
+
+    //------------------------------------------------//
+    
+    outFile->cd();
+    outFile->Write();
+    outFile->Close();
+
+    return 0;
+}
